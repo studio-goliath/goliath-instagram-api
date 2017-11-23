0284e16ef8fd13958593cf36a6017c28d49d1f29
ArnaudBan <abanvillet@studio-goliath.com>
2017-11-23 15:34:43 +0100

    Add real class to call API

--- goliath-instagram-api.php
+++ goliath-instagram-api.php
@@ -11,54 +11,13 @@
 // Option page
 require_once( plugin_dir_path( __FILE__ ) . '/admin/instagram-options.php' );
 
-
-// Instagram Stuff
-function gins_generate_sig($endpoint, $params, $secret) {
-
-    $sig = $endpoint;
-    ksort($params);
-
-    foreach ($params as $key => $val) {
-        $sig .= "|$key=$val";
-    }
-
-    return hash_hmac('sha256', $sig, $secret, false);
-}
-
-function gins_get_instagram_last_picture(){
-
-    $instagram_last_picture = get_transient( 'instagram_last_picture' );
-
-    if( ! $instagram_last_picture ){
-
-        $endpoint = '/users/self/media/recent';
-        $params = array(
-          'access_token' => get_option( 'gins_client_access_token' ),
-          'count' => 5,
-        );
-        $secret = get_option( 'gins_client_info_secret' );
-
-        $sig = gins_generate_sig($endpoint, $params, $secret);
-        $params['sig'] = $sig;
-
-        $url = add_query_arg( $params, 'https://api.instagram.com/v1' . $endpoint);
-
-        $instagram_last_picture_get = wp_remote_get( $url );
-
-        if( ! is_wp_error( $instagram_last_picture_get ) ){
-
-            $instagram_last_picture_body = json_decode( wp_remote_retrieve_body( $instagram_last_picture_get ) );
-
-            if( $instagram_last_picture_body->meta->code != 400 ){
-
-                $instagram_last_picture = $instagram_last_picture_body;
-
-                set_transient( 'instagram_last_picture', $instagram_last_picture, HOUR_IN_SECONDS );
-            }
+require_once( plugin_dir_path( __FILE__ ) . '/inc/Gins_Api_Call.php' );
 
 
-        }
-    }
+function gins_instagram_self_recent_media(){
 
-    return $instagram_last_picture;
+    $inst_api_call = new Gins_Api_Call();
+
+    return $inst_api_call->call( '/users/self/media/recent' );
+
 }
---
+++ inc/Gins_Api_Call.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: goliath
+ * Date: 23/11/2017
+ * Time: 11:06
+ */
+
+Class Gins_Api_Call
+{
+
+    private $instagram_api_url;
+
+    private $client_access_token;
+
+    private $client_info_secret;
+
+    public function __construct()
+    {
+
+        $this->instagram_api_url = 'https://api.instagram.com/v1';
+
+        $this->client_access_token = get_option( 'gins_client_access_token' );
+
+        $this->client_info_secret = get_option( 'gins_client_info_secret' );
+
+    }
+
+    public function call( $endpoint, $params = array() )
+    {
+
+        $params = wp_parse_args(
+            $params,
+            array(
+                'access_token'  =>  $this->client_access_token,
+                'count'         => 10
+            )
+        );
+
+        $transient_key = $this->get_transient_key( $endpoint, $params );
+
+        $response = get_transient( $transient_key );
+
+        if( ! $response ){
+
+            $params['sig'] = $this->generate_sig($endpoint, $params);
+
+            $url = add_query_arg( $params, $this->instagram_api_url . $endpoint);
+
+            $response = wp_remote_get( $url );
+
+            if( ! is_wp_error( $response ) ){
+
+
+                if( wp_remote_retrieve_response_code( $response ) == 200 ){
+
+                    $response = json_decode( wp_remote_retrieve_body( $response ) );
+
+                    set_transient( $transient_key, $response, HOUR_IN_SECONDS );
+                }
+
+
+            }
+        }
+
+        return $response;
+
+    }
+
+
+    /**
+     * Generate call signature
+     *
+     * @param string $endpoint
+     * @param array $params
+     *
+     * @return false|string
+     */
+    private function generate_sig($endpoint, $params)
+    {
+
+        $sig = $endpoint;
+        ksort($params);
+
+        foreach ($params as $key => $val) {
+            $sig .= "|$key=$val";
+        }
+
+        return hash_hmac('sha256', $sig, $this->client_info_secret, false);
+    }
+
+
+    /**
+     * Get transient key
+     *
+     * @param string $endpoint
+     * @param array $params
+     *
+     * @return string
+     */
+    private function get_transient_key( $endpoint, $params) {
+
+        $transient_key_md5 = md5( $endpoint . ',' . implode( ',', $params ) );
+        return "_gins_{$transient_key_md5}";
+
+    }
+}
