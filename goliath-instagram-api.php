6cc39c37d6d1a374c8769caebfdfee125aae3d16
ArnaudBan <abanvillet@studio-goliath.com>
2018-01-17 17:45:49 +0100

    Allow to add params to the query

--- goliath-instagram-api.php
+++ goliath-instagram-api.php
@@ -14,10 +14,10 @@ require_once( plugin_dir_path( __FILE__ ) . '/admin/instagram-options.php' );
 require_once( plugin_dir_path( __FILE__ ) . '/inc/Gins_Api_Call.php' );
 
 
-function gins_instagram_self_recent_media(){
+function gins_instagram_self_recent_media( $params = array()){
 
     $inst_api_call = new Gins_Api_Call();
 
-    return $inst_api_call->call( '/users/self/media/recent' );
+    return $inst_api_call->call( '/users/self/media/recent', $params );
 
 }
